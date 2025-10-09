396a10befba5b043c3be2ac52e634e8f32c76898
SafalBhandari12 <safalbhandari069@gmail.com>
2025-10-09 18:34:28 +0530

    added href

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -27,6 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        {/* Ahrefs Analytics */}
+        <Script
+          src='https://analytics.ahrefs.com/analytics.js'
+          data-key='MHJgHCy3P4YBRgUTz+obIA'
+          strategy='afterInteractive'
+        />
+      </head>
       <body className={`${inter.variable} ${playfair.variable} antialiased`}>
         {children}
       </body>
